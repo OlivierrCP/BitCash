1cb2f414af8acf8008431263c154c59e6e22aedd
Olivie <34624398+OlivierrCP@users.noreply.github.com>
2018-06-28 11:00:30 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    var ampm = "";
+    m = checkTime(m);
 
+    if (h > 12) {
+    	h = h - 12;
+    	ampm = " PM";
+    } else if (h == 12){
+        h = 12;
+    	ampm = " AM";
+    } else if (h < 12){
+        ampm = " AM";
+    } else {
+        ampm = "PM";
+    };
+  
+  
